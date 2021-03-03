2554ad9c5b1cc3e5cccb740c3a15d43154f21115
煮酒泛舟 <lixhbs@163.com>
2021-03-03 11:55:30 +0800

    集成nacos配置中心

--- user-service/src/main/java/com/chengyu/UserApplication.java
+++ user-service/src/main/java/com/chengyu/UserApplication.java
@@ -3,6 +3,7 @@ package com.chengyu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author lix.
