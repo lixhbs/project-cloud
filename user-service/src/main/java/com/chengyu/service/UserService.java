956cf5c35fa9197085dea08755a9d3913046ac2f
煮酒泛舟 <lixhbs@163.com>
2021-03-14 13:15:20 +0800

    注册校验邮箱是否注册、添加拦截器

--- user-service/src/main/java/com/chengyu/service/UserService.java
+++ user-service/src/main/java/com/chengyu/service/UserService.java
@@ -10,9 +10,30 @@ import com.chengyu.util.JsonData;
  */
 public interface UserService
 {
-   UserDO detail(String id);
+    /**
+     * 查询用户详情
+     * @author Lix.
+     * @param id id
+     * @return UserDO UserDO
+     * @date 2021/3/14 13:13
+     */
+    UserDO detail(String id);
 
-   JsonData register(UserRegisterRequest registerRequest);
+    /**
+     * 用户注册
+     * @author Lix.
+     * @param registerRequest registerRequest
+     * @return JsonData
+     * @date 2021/3/14 13:13
+     */
+    JsonData register(UserRegisterRequest registerRequest);
 
+    /**
+     * 用户登录
+     * @author Lix.
+     * @param loginRequest loginRequest
+     * @return JsonData
+     * @date 2021/3/14 13:13
+     */
     JsonData login(UserLoginRequest loginRequest);
 }
